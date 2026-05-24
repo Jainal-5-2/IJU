d119a63e7ba8352780b4160ffbe25c4b7005b79c
root <asbijay@gmail.com>
2026-05-24 20:39:07 +0800

    Trying to publish

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Landing from './pages/Landing';
 import SchoolRegister from './pages/SchoolRegister';
